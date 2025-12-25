f15c0e934697929fbe865a25c2046b0fec263b15
Liu Jiang <liujiang@example.com>
2025-12-25 21:18:49 +0800

    chore: 强化学习部分需要重新找资料学习，暂且转战docker实现，

---
+++ chapter7-4-DQN/model.py
@@ -0,0 +1,10 @@
+# chapter7-4-DQN/model.py
+
+import torch
+import torch.nn as nn
+
+
+class CartPoleDQN(nn.Module):
+    def __init__(self):
+        super(CartPoleDQN, self).__init__()
+        pass
